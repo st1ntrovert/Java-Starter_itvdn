c2ac321671504bed108cf1ee62524975a9e5fb6d
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-05-26 10:32:23 +0200

    ITVDN. Java Starter. Lesson 7. Home task #2 done.

--- src/Arithmetics.java
+++ src/Arithmetics.java
@@ -1,2 +1,28 @@
 public class Arithmetics {
+    static int result;
+
+    static void add(int a, int b) {
+        result = a + b;
+        System.out.println("Result is " + result);
+    }
+
+    static void sub(int a, int b) {
+        result = a - b;
+        System.out.println("Result is " + result);
+    }
+
+    static void mul(int a, int b) {
+        result = a * b;
+        System.out.println("Result is " + result);
+    }
+
+    static void div(int a, int b) {
+        if (b == 0) {
+            System.out.println("You can't divide by zero!");
+        } else {
+            result = a / b;
+            System.out.println("Result is " + result);
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Calculator.calculate(32, 9, 4));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose operation: +, -, * or / :");
+        switch (input.nextLine()) {
+            case "+":
+                System.out.println("Enter the first number first, then the second.");
+                Arithmetics.add(input.nextInt(), input.nextInt());
+                break;
+            case "-":
+                System.out.println("Enter the first number first, then the second.");
+                Arithmetics.sub(input.nextInt(), input.nextInt());
+                break;
+            case "*":
+                System.out.println("Enter the first number first, then the second.");
+                Arithmetics.mul(input.nextInt(), input.nextInt());
+                break;
+            case "/":
+                System.out.println("Enter the first number first, then the second.");
+                Arithmetics.div(input.nextInt(), input.nextInt());
+                break;
+            default:
+                System.out.println("Please, Try Again :)");
+                break;
+        }
+
     }
+
 }
