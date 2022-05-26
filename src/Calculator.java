fa5a58f3a01f7f47d0c7521c3d2f855d3621f55b
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-05-26 09:51:29 +0200

    ITVDN. Java Starter. Lesson 7. Additional task done.

---
+++ src/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    static double calculate(int a, int b, int c) {
+        double average = (double) (a + b + c) / 3;
+        return average;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.calculate(32, 9, 4));
+    }
+}
