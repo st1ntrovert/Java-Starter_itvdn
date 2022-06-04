51fa94001b82fb541212c4e461020bdaa67f7438
Oleksandr Bondarevskyi <epickonst@gmail.com>
2022-06-04 13:11:10 +0200

    ITVDN.com | Java Starter. Lesson 8. Additional task.

---
+++ src/Calculator2.java
@@ -0,0 +1,12 @@
+public class Calculator2 {
+    static void calculate(double a, double b, double c) {
+        String result = "a = " + a / 5 + ", b = " + b / 5 + ", c = " + c / 5;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator2.calculate(6, 39, 45);
+    }
+}
+
+
